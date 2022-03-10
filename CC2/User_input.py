a407614ea4dca0693e47b992e59656f679422822
Lincoln Dark <lincoln_dark@uri.edu>
2022-03-10 15:56:37 -0500

    rwtg

---
+++ CC2/User_input.py
@@ -0,0 +1,6 @@
+age = input("What is your age? ")
+print("Your age is " + str(age))
+print("You will retire in" + str(65 - age) + "years")
+#source https://www.practicepython.org/solution/2014/02/05/01-character-input-solutions.html
+
+#AD code failed, python2 print statements used, npt python3
