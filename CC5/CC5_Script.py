540d31f563c7bf53afe32c916623c64f50080d36
Lincoln Dark <lincoln_dark@uri.edu>
2022-03-10 15:36:31 -0500

    Update CC5_Script.py
    update

--- CC5/CC5_Script.py
+++ CC5/CC5_Script.py
@@ -14,15 +14,14 @@ saved_layer = r"osprey_polygon.shp"
 spRef = arcpy.SpatialReference(4326)
 #extracting by unique year
 years = []
-test = open(str(arcpy.env.workspace + r'\CC5_CSV.csv'), newline='')
-testreader = csv.reader(test, delimiter=',')
-linecount=0
-for row in testreader:
-    if row[2] not in years:
-        years.append(row[2])
-years.pop(0)
+with open(in_table) as the_csv:
+    headerline = the_csv.next()
+    for row in csv.reader(the_csv):
+        if row[2] not in years:
+            years.append(row[2])
 print(len(years))
 print(years)
+
 #test
 # making new CSVs by year
 # os.mkdir("Years_Directory")
@@ -35,37 +34,7 @@ print(years)
 #                 file = open(r"Years_Directory/" + str(year) + ".csv", "w")
 #                 file.write(header)
 #                 year_count = 0
-#     file.close()
 
-
-for year in years:
-    with open('%s-output.csv' % year, 'w', newline='') as next_csv:
-        csv_creator = csv.writer(next_csv)
-        csv_creator.writerow(['lat', 'long', 'year'])
-        for row in testreader:
-            if row[2] == year:
-                csv_creator.writerow(row[0])
-
-
-# with open(arcpy.env.workspace + r'\CC5_CSV.csv') as the_csv:
-#     headerline = the_csv.next()
-#     for row in csv.reader(the_csv):
-#         if row[0] not in years:
-#             years.append(row[0])
-# print(len(years))
-# os.mkdir("Years_Directory")
-# header = "lat,long,year"
-# years = [2010,2020]
-# # for year in years:
-# #     year_list = 1
-# #     with open("CC5_CSV.csv") as Table:
-# #         for row in csv.reader(Table):
-# #             if row[0] == year:
-# #                 if year_list == 1:
-# #                     file = open(r"Years_Directory/" + str(year) + ".csv", "w")
-# #                     file.write(header)
-# #                     year_list = 0
-# #     file.close()
 # os.rmdir("Years_Directory")
 
 
