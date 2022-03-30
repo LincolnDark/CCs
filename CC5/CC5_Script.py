eec2701dd5b21a3d516b48cb995be75feb33e94d
Lincoln Dark <lincoln_dark@uri.edu>
2022-03-30 12:05:54 -0400

    updates

--- CC5/CC5_Script.py
+++ CC5/CC5_Script.py
@@ -1,3 +1,5 @@
+# First, we must import packages that we will need for our code to function, as well as set up the directories to tell
+# our code where to pull files from as well as store files that we will create.
 import csv
 import arcpy
 import os
@@ -7,7 +9,8 @@ outputDirectory = "Years_Directory"
 if not os.path.exists(os.path.join(directory, outputDirectory)):
     os.mkdir(os.path.join(directory, outputDirectory))
 
-# making a list of years
+# Second, we will make a list of years using our CSV full of data. This will allow us to make two seperate CSVs, 1 for
+# each year, in the next step.
 year_list = []
 with open(os.path.join(directory, r"CC5_CSV.csv")) as the_csv:
     csv_reader = csv.reader(the_csv)
@@ -18,7 +21,8 @@ with open(os.path.join(directory, r"CC5_CSV.csv")) as the_csv:
             year_list.append(row[2])
 print(year_list)
 
-# making a csv for each year
+# Using our list of unique years, we will make two seperate CSVs, one for each year. Doing this will allow us to
+# create a shapefile for each year in the next step.
 for year in year_list:
     with open(os.path.join(directory, r"CC5_CSV.csv")) as the_csv:
         csv_reader = csv.reader(the_csv)
@@ -30,7 +34,8 @@ for year in year_list:
                 file.write("\n")
         file.close()
 
-    # making two shapefiles
+    # Now that we have a CSV for each year, we can use each CSV to make a shapefile for the data that is associated
+    # with that year.
     in_Table = os.path.join(directory, outputDirectory, str(year) + ".csv")
     x = "long"
     y = "lat"
@@ -42,7 +47,7 @@ for year in year_list:
     print(arcpy.GetCount_management(out_Layer))
     arcpy.CopyFeatures_management(lyr, saved_Layer)
     if arcpy.Exists(saved_Layer):
-        print("Good Job, Lincoln!")
+        print("The shapefile exists!")
 
     desc = arcpy.Describe(saved_Layer)
     XMin = desc.extent.XMin
@@ -50,13 +55,14 @@ for year in year_list:
     YMin = desc.extent.YMin
     YMax = desc.extent.YMax
 
-# creating a fishnet for each shapefile
+# This code will create a fishnet for the points in each shapefile. Making a fishnet will allow us to create our
+# heatmap, which visualizes the density of points in a gridcell of the fishnet.
     arcpy.env.outputCoordinateSystem = arcpy.SpatialReference(4326)
     outFeatureClass = os.path.join(directory, outputDirectory, str(year) + "_Osprey_nest_locations.shp")
     originCoordinate = str(XMin) + " " + str(YMin)
     yAxisCoordinate = str(XMin) + " " + str(YMin + 1)
-    cellSizeWidth = "0.25"
-    cellSizeHeight = "0.25"
+    cellSizeWidth = "0.125"
+    cellSizeHeight = "0.125"
     numRows = ""
     numColumns = ""
     oppositeCorner = str(XMax) + " " + str(YMax)
@@ -69,7 +75,8 @@ for year in year_list:
     if arcpy.Exists(outFeatureClass):
         print("Created " +str(year) + " fishnets!")
 
-# joining fishnets to points
+# In this step, we join the points from each layer to their fishnet. This places the fishnet in the correct place
+# on the map.
 
     target_features = os.path.join(directory, outputDirectory, str(year) + "_Osprey_nest_locations.shp")
     join_features = os.path.join(directory, outputDirectory, str(year) + "Osprey_map.shp")
