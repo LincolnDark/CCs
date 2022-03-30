6950426ffeb565ff2e7d2d4df3fecbb2787896b0
Lincoln Dark <lincoln_dark@uri.edu>
2022-03-30 11:49:16 -0400

    update

--- CC4/Use of Tools.py
+++ CC4/Use of Tools.py
@@ -3,20 +3,9 @@
 # Lincoln Dark - March 2022
 
 import arcpy
-
-def Model():
-    arcpy.env.overwriteOutput = False
-    arcpy.CheckOutExtension("spatial")
-    with arcpy.EnvManager(scratchWorkspace=r"C:\Users\14017\Documents\ArcGIS\Projects\FixingCC4\FixingCC4.gdb", workspace=r"C:\Users\14017\Documents\ArcGIS\Projects\FixingCC4\FixingCC4.gdb"):
-        NLCD_2001_img = arcpy.Raster("NLCD_2001.img")
-        Shrink_NLCD_1 = "C:\\Users\\14017\\Documents\\ArcGIS\\Projects\\FixingCC4\\FixingCC4.gdb\\Shrink_NLCD_1"
-        Shrink = Shrink_NLCD_1
-        with arcpy.EnvManager(scratchWorkspace=r"C:\Users\14017\Documents\ArcGIS\Projects\FixingCC4\FixingCC4.gdb", workspace=r"C:\Users\14017\Documents\ArcGIS\Projects\FixingCC4\FixingCC4.gdb"):
-            Shrink_NLCD_1 = arcpy.sa.Shrink(in_raster=NLCD_2001_img, number_cells=1, zone_values=[41, 42, 43, 90], shrink_method="MORPHOLOGICAL")
-            Shrink_NLCD_1.save(Shrink)
+arcpy.env.overwriteOutput = True
+arcpy.env.workspace = r"C:\Users\14017\Desktop\NRS_528\Github\CC4"
+Outshrink = arcpy.sa.Shrink("NLCD_2001.img", 1, [41,42, 43, 90])
+Outshrink.save("NLCD_2001_shrink.img")
 
 
-if __name__ == '__main__':
-    Model()
-
-# code generated using ArcGIS Pro Modelbuilder
