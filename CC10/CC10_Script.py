19976741f525c6988cb467cff87f2c6c8efa7832
Lincoln Dark <lincoln_dark@uri.edu>
2022-04-18 10:21:13 -0400

    Update CC10_Script.py

--- CC10/CC10_Script.py
+++ CC10/CC10_Script.py
@@ -4,7 +4,13 @@ import arcpy, os
 arcpy.env.workspace = r"C:\Users\14017\Desktop\NRS_528\Github\CC10\Landsat_data_lfs\Step_2_data\Winter_2015"
 
 # The next step is to composite the bands we are interested in, in this case, bands 4 & 5.
-arcpy.CompositeBands_management(in_rasters=r"C:\Users\14017\Desktop\NRS_528\Github\CC10\Landsat_data_lfs\Step_2_data\Winter_2015\LC08_L1TP_012031_20150201_20170301_01_T1_B4.tif;"
-                                           r"C:\Users\14017\Desktop\NRS_528\Github\CC10\Landsat_data_lfs\Step_2_data\Winter_2015\LC08_L1TP_012031_20131212_20170307_01_T1_B5.tif",
-                                out_raster=r"C:\Data\Course_ArcGIS_Python\Classes\06_Cheating\DataFolder_Step_2_data_lfs\Step_2_data\winter15")
+Composite_Raster = arcpy.CompositeBands_management(in_rasters=
+                                                    r"C:\Users\14017\Desktop\NRS_528\Github\CC10\Landsat_data_lfs\
+                                                    Step_2_data\Winter_2015\
+                                                    LC08_L1TP_012031_20150201_20170301_01_T1_B4.tif;"
+                                                    r"C:\Users\14017\Desktop\NRS_528\Github\CC10\Landsat_data_lfs\
+                                                    Step_2_data\Winter_2015\
+                                                    LC08_L1TP_012031_20150201_20170301_01_T1_B5.tif",
+                                out_raster=r"C:\Data\Course_ArcGIS_Python\Classes\06_Cheating\
+                                DataFolder_Step_2_data_lfs\Step_2_data\winter15")
 
