89e47df379ee38e8e631decd3c333136ffa943d4
Lincoln Dark <lincoln_dark@uri.edu>
2022-04-18 10:01:46 -0400

    update

---
+++ CC10/CC10_Script.py
@@ -0,0 +1,10 @@
+# Lincoln Dark - Coding Challenge 10 - April 18th, 2022
+# The first step is importing necessary packages and setting our workspace, so our script knows where to find the data
+import arcpy, os
+arcpy.env.workspace = r"C:\Users\14017\Desktop\NRS_528\Github\CC10\Landsat_data_lfs\Step_2_data\Winter_2015"
+
+# The next step is to composite the bands we are interested in, in this case, bands 4 & 5.
+arcpy.CompositeBands_management(in_rasters=r"C:\Users\14017\Desktop\NRS_528\Github\CC10\Landsat_data_lfs\Step_2_data\Winter_2015\LC08_L1TP_012031_20150201_20170301_01_T1_B4.tif;"
+                                           r"C:\Users\14017\Desktop\NRS_528\Github\CC10\Landsat_data_lfs\Step_2_data\Winter_2015\LC08_L1TP_012031_20131212_20170307_01_T1_B5.tif",
+                                out_raster=r"C:\Data\Course_ArcGIS_Python\Classes\06_Cheating\DataFolder_Step_2_data_lfs\Step_2_data\winter15")
+
